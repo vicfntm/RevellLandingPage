0d4a9f0f3569ebcf9ff2d09279e0b1e531e2f7eb
Vic <4944995@gmail.com>
2016-07-09 21:52:12 +0300

    09/07/16 evening

---
+++ js/jquery.hidegallery.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function () {
+    $("#starwars").click(function () {
+        $("#cars").fadeOut();
+        $("#planes").fadeOut();
+        $("#tanks").fadeOut();
+        $("#ships").fadeOut();
+        $("#sw").fadeIn("slow");
+    })
+    $("#crs").click(function () {
+        $("#sw").fadeOut();
+        $("#planes").fadeOut();
+        $("#tanks").fadeOut();
+        $("#ships").fadeOut();
+        $("#cars").fadeIn("slow");
+    })
+    $("#shps").click(function () {
+        $("#sw").fadeOut();
+        $("#planes").fadeOut();
+        $("#tanks").fadeOut();
+        $("#cars").fadeOut();
+        $("#ships").fadeIn("slow");
+    })
+    $("#plns").click(function () {
+        $("#sw").fadeOut();
+        $("#cars").fadeOut();
+        $("#tanks").fadeOut();
+        $("#ships").fadeOut();
+        $("#planes").fadeIn("slow");
+    })
+})
